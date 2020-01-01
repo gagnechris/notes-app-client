44b348c0e105ef50cc8bc04aac363b48e8a739b2
Chris Gagne <gagnechris@me.com>
2020-01-01 14:43:31 -0500

    Setup react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-uploads-kehi8xvt"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://ajz1z6dfpe.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_sNyW42tZZ",
+    APP_CLIENT_ID: "quh6t697mv11lanea4d8pj5v6",
+    IDENTITY_POOL_ID: "us-east-1:d9d32c65-2498-4bb5-b4bb-912bac76a253"
+  }
+};
